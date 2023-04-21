a29f31cdb13c70f7fbeffed2f810d599ac789767
kk9six <account@github.kksix.me>
2023-04-21 22:23:44 +0900

    update create_sliding_window_features: fix window size

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import numpy as np
 
-def create_sliding_window_features(X, y, window_sizes):
+def create_sliding_window_features(X, y, window_size=50):
     """
         X: DataFrame
         window_sizes: list of tuple [('col name', size), ..., ('col name', size)]
     """
-    for col, window_size in window_sizes:
-        for i in range(window_size):
-            X[f"{col}_lag_{i+1}"] = X[col].shift(i + 1)
-    start_index = np.intersect1d(y.index.tolist(), X.dropna().index.tolist()).min()
+    new_cols = [X.shift(i) for i in range(window_size+1)]
+    new_X = pd.concat(new_cols, axis=1).dropna()
+    start_index = np.intersect1d(y.index.tolist(), new_X.index.tolist()).min()
 
-    return X.dropna().loc[y.loc[start_index].index.tolist()], y.loc[start_index:]
+    return new_X.loc[y.loc[start_index].index.tolist()], y.loc[start_index:]
