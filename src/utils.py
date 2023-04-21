b95eae619120b59fad38d2c5cac851af9d6833cd
kk9six <account@github.kksix.me>
2023-04-21 21:44:43 +0900

    update create_sliding_window_features: input list of tuple

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 
-def create_sliding_window_features(X, y, window_size, target_col):
+def create_sliding_window_features(X, y, window_sizes):
     """
         X: DataFrame
-        target_col: col name
+        window_sizes: list of tuple [('col name', size), ..., ('col name', size)]
     """
-    for i in range(window_size):
-        X[f"lag_{i+1}"] = X[target_col].shift(i + 1)
+    for col, window_size in window_sizes:
+        for i in range(window_size):
+            X[f"{col}_lag_{i+1}"] = X[col].shift(i + 1)
     start_index = np.intersect1d(y.index.tolist(), X.dropna().index.tolist()).min()
 
     return X.dropna().loc[y.loc[start_index].index.tolist()], y.loc[start_index:]
