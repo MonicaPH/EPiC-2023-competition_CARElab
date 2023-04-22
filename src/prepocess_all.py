b6865c8b2494449c780734ffbf917182030f23bd
Felix <felix.dollack@googlemail.com>
2023-04-23 00:33:14 +0900

    Remove debug print and revert export to csv

--- src/prepocess_all.py
+++ src/prepocess_all.py
@@ -5,7 +5,6 @@ import shutil
 
 if __name__ == '__main__':
     p = Path(__file__).parent / Path('../data')
-    print(p)
     for x in p.rglob('*'):
         for file in x.glob('*.csv'):
             # prepare output directory
@@ -16,7 +15,7 @@ if __name__ == '__main__':
             if not 'annotations' in str(file):
                 # clean and save data
                 df = clean(file)
-                df.to_parquet(outPath / file.name[:-3]+'parquet', index=False)
+                df.to_csv(outPath / file.name, index=False)
             else:
                 # copy annotation
                 shutil.copy(file, outPath / file.name)
