299898e4a865dbbcccb4632f60b1dcd0891174e8
MonicaPH <MonicaPH@users.noreply.github.com>
2023-04-23 15:56:39 +0900

    Fix emg processing

--- src/emg.py
+++ src/emg.py
@@ -2,9 +2,12 @@ import neurokit2 as nk
 import numpy as np
 import pandas as pd
 import scipy
+from scipy import signal
+import more_itertools
+from scipy.stats import zscore
 
 
-def process(emg_signal, sampling_rate=1000, filterCutoff=100):
+def emg_process(emg_signal, sampling_rate=1000, filterCutoff=5):
 
     # Sanitize input
     emg_signal = nk.signal_sanitize(emg_signal)
@@ -12,6 +15,25 @@ def process(emg_signal, sampling_rate=1000, filterCutoff=100):
     # Clean signal
     # emg_cleaned = emg_clean(emg_signal, sampling_rate=sampling_rate)
     emg_cleaned = emg_clean(emg_signal, filterCutoff=filterCutoff, sampling_rate=sampling_rate)
+
+    # Windowing
+    step_size = 1 #slide
+    window_size = int(round(0.1*sampling_rate)) #51 #window of 100ms at 512Hz = 51 samples
+    # calculate z-scores
+    emg_z = zscore(emg_cleaned)
+    # calculate RMS
+    rms_data = []
+    windowed_data = list(more_itertools.windowed(emg_z, window_size, step=step_size))
+    leftover_size = len(emg_z) - len(windowed_data)
+    for window in windowed_data:
+        rms_data.append(np.sqrt(np.square(window).mean(axis=0)))
+    rms_data.extend([0]*leftover_size)
+    # low pass to further remove noise from the rms
+    savGolWinLen = sampling_rate
+    if (sampling_rate%2)==0:
+        # savgol window length must be odd
+        savGolWinLen = sampling_rate+1
+    rms_data = signal.savgol_filter(rms_data, savGolWinLen, 3)
 
     # Get amplitude
     amplitude = nk.emg_amplitude(emg_cleaned)
@@ -24,7 +46,7 @@ def process(emg_signal, sampling_rate=1000, filterCutoff=100):
 
     # Prepare output
     signals = pd.DataFrame(
-        {"EMG_Raw": emg_signal, "EMG_Clean": emg_cleaned, "EMG_Amplitude": amplitude}
+        {"EMG_Raw": emg_signal, "EMG_Clean": emg_cleaned, "EMG_Rms": rms_data, "EMG_Amplitude": amplitude}
     )
 
     signals = pd.concat([signals, activity_signal], axis=1)
@@ -32,8 +54,13 @@ def process(emg_signal, sampling_rate=1000, filterCutoff=100):
     return signals, info
 
 
-def emg_clean(emg_signal, sampling_rate=1000, filterCutoff=100):
+def emg_clean(emg_signal, sampling_rate=1000, filterCutoff=5):
     emg_signal = nk.as_vector(emg_signal)
+
+    f_notch = np.array([60, 120, 180, 240]) # notch frequencies to filter out because of line noise
+    width   = np.ones(len(f_notch)) * 3;              # width/2 of each notch
+    cutlow  = 5    # lower cut-off frequency for elliptic filter
+    cuthigh = 250  # upper cut-off frequency for elliptic filter
 
     # Missing data
     n_missing = np.sum(np.isnan(emg_signal))
@@ -46,18 +73,21 @@ def emg_clean(emg_signal, sampling_rate=1000, filterCutoff=100):
         emg_signal = _emg_clean_missing(emg_signal)
 
     # Parameters
-    order = 4
-    frequency = filterCutoff
-    frequency = (
-        2 * np.array(frequency) / sampling_rate
-    )  # Normalize frequency to Nyquist Frequency (Fs/2).
+    # order = 4
+    # frequency = filterCutoff
+    # frequency = (
+    #     2 * np.array(frequency) / sampling_rate
+    # )  # Normalize frequency to Nyquist Frequency (Fs/2).
 
-    # Filtering
-    b, a = scipy.signal.butter(N=order, Wn=frequency, btype="highpass", analog=False)
-    filtered = scipy.signal.filtfilt(b, a, emg_signal)
+    # filter it
+    clean = local_filter(emg_signal, f_notch, width, cutlow, cuthigh, sampling_rate)
 
-    # Baseline detrending
-    clean = nk.signal_detrend(filtered, order=0)
+    # # Filtering
+    # b, a = scipy.signal.butter(N=order, Wn=frequency, btype="highpass", analog=False)
+    # filtered = scipy.signal.filtfilt(b, a, emg_signal)
+
+    # # Baseline detrending
+    # clean = nk.signal_detrend(filtered, order=0)
 
     return clean
 
@@ -69,3 +99,29 @@ def _emg_clean_missing(emg_signal):
     emg_signal = pd.DataFrame.pad(pd.Series(emg_signal))
 
     return emg_signal
+
+# Functions taken from matlab implementation following Gruebler 2014
+def notch_filter(data, notch, width, fs):
+    fa = (notch - width) / (fs/2)
+    fb = (notch + width) / (fs/2)
+    Wn = np.array([fa, fb])
+    order = 4
+    b, a = signal.butter(order, Wn, btype='stop')
+    return signal.filtfilt(b, a, data)
+
+def elliptic_filter(data, flow, fhigh, fs):
+    Wn    = np.array([flow, fhigh]) * (2/fs)
+    order = 4
+    maxRipple      = 0.1
+    minAttenuation = 40
+    b, a  = signal.ellip(order, maxRipple, minAttenuation, Wn, btype='stop')
+    return signal.filtfilt(b, a, data)
+
+def local_filter(data, notch, width, flow, fhigh, fs):
+    if (len(notch) > 1):
+        for kk in range(len(notch)):
+            data = notch_filter(data, notch[kk], width[kk], fs)
+    else:
+        data = notch_filter(data, notch, width, fs)
+    data = elliptic_filter(data, flow, fhigh, fs)
+    return signal.detrend(data)
