a65c7255f4ce759b4851161c4d5b3d050a79219c
kk9six <account@github.kksix.me>
2023-04-26 02:39:53 +0900

    rename

--- io_data/s1_train.py
+++ io_data/s1_train.py
@@ -2,7 +2,4 @@ import os, sys
 sys.path.append(os.path.relpath("../src/"))
 from io_generator import save_training_io
 
-# save_training_io(1)
-save_training_io(2)
-# save_training_io(3)
-save_training_io(4)
+save_training_io(1)
