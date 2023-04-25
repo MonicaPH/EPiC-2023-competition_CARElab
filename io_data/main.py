571de4c3b22590b03c5b05f9a159c2264efcb788
kk9six <account@github.kksix.me>
2023-04-25 15:59:33 +0900

    generate io data for S2, 4

--- io_data/main.py
+++ io_data/main.py
@@ -2,4 +2,7 @@ import os, sys
 sys.path.append(os.path.relpath("../src/"))
 from io_generator import save_training_io
 
-save_training_io(1)
+# save_training_io(1)
+save_training_io(2)
+# save_training_io(3)
+save_training_io(4)
