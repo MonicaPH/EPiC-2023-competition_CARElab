20ad34980db854eb81ef568d2a9acf8a71a5cc0b
Hideaki Uchiyama <hideaki.uchiyama.tot@gmail.com>
2023-04-25 23:28:33 +0900

    fix a minor issue

--- models/train_s2_250.py
+++ models/train_s2_250.py
@@ -46,7 +46,7 @@ prefix = '../'
 #
 scenario = 2
 subs = [30, 31, 32, 34]
-num_gpus = 2
+num_gpus = 1
 
 input_path = Path(prefix) / f'io_data/scenario_{scenario}' / 'train' / 'physiology'
 output_path = Path(prefix) / f'io_data/scenario_{scenario}' / 'train' / 'annotations'
