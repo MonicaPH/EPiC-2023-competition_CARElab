554b64752c261f3bc04fb896ca863810026208b5
Felix <felix.dollack@googlemail.com>
2023-04-25 16:28:01 +0200

    Update torch dataset to use all sensors by default but allow selection

--- src/dataset.py
+++ src/dataset.py
@@ -10,23 +10,39 @@ class EpicDataset(Dataset):
     trainScenario1Dataset = EpicDataset(S1())
     testScenario1Dataset = EpicDataset(S1(), is_test=True)
     """
-    def __init__(self, scenario, modality, windowLen=50, is_test=False):
+    def __init__(
+        self, scenario,
+        modality:list=[
+            'ecg', 'bvp', 'gsr', 'rsp', 'skt',
+            'emg_zygo', 'emg_coru', 'emg_trap'
+        ],
+        offset:int=0, windowLen:int=50, is_test:bool=False
+    ):
+        if ('emg' in modality):
+            tmp = [x for x in modality if not 'emg' in x]
+            tmp.extend(['emg_zygo', 'emg_coru', 'emg_trap'])
+            modality = tmp
+
         self.is_test = is_test
         self.loader = scenario
-        self.data = np.zeros((windowLen,1))
-        self.label = np.zeros((1,2))
+        self.data = np.zeros((windowLen, 1, len(modality)))
+        self.label = np.zeros((1, 2))
         self.numBlocks = 0
-        for sub, vid in [
+        for kk, (sub, vid) in enumerate([
             x[(not self.is_test)*'train'+self.is_test*'test']
             for x in scenario.train_test_groups()
-        ]:
+        ]):
             data, label = self.loader.train_data(sub, vid)
+            if (kk < 1):
+                data = data[offset:]
             numBlocks = len(data) // windowLen
             endIdx = windowLen * numBlocks
             # rearrange data and annotations
             self.data = np.concatenate([
                 self.data,
-                data[modality].values[:endIdx].reshape(windowLen, -1)
+                data[:endIdx][modality].values.reshape(
+                    windowLen, -1, len(modality)
+                )
             ], axis=1)
             self.label = np.concatenate([
                 self.label,
@@ -41,4 +57,4 @@ class EpicDataset(Dataset):
         return self.numBlocks-1 # 1 dummy entry at the beginning to concatenate with 0's
 
     def __getitem__(self, idx):
-        return self.data[:, 1+idx], self.label[1+idx, :]
+        return self.data[:, 1+idx, :], self.label[1+idx, :]
