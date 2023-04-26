c328844074344f6f483563b8eb21f39281cd97b6
kk9six <account@github.kksix.me>
2023-04-26 17:36:04 +0900

    strategy 2 per vid

---
+++ io_data/s2_train_v2.py
@@ -0,0 +1,48 @@
+import os, sys
+import multiprocessing
+import pandas as pd
+from functools import partial
+from pathlib import Path
+sys.path.append(os.path.relpath("../src/"))
+from io_generator import load_data_dict, load_io
+from utils import check_dir
+
+model_list = []
+sub_list = []
+past_window_size = 50
+future_window_size = 50
+
+data_dict = load_data_dict()
+for fold in data_dict['folds'][1]:
+    for vid in data_dict[2][fold]['train_vids']:
+        model_list.append([(fold, sub, vid) for sub in data_dict[2][fold]['train_subs']])
+            
+prefix = '../'
+input_path = Path(prefix) / f'io_data/scenario_2/train/physiology'
+output_path = Path(prefix) / f'io_data/scenario_2/train/annotations'
+check_dir(input_path, output_path)
+
+def func(fold_sub_vid_pairs, input_path, output_path, past_window_size, future_window_size):
+    Xs = None
+    ys = None
+    v = 0
+    f = 0
+    for fold, sub, vid in fold_sub_vid_pairs:
+        f = fold
+        v = vid 
+        X, y = load_io(2, fold, sub, vid, 'train', '../', past_window_size=past_window_size, future_window_size=future_window_size)
+        Xs = pd.concat([Xs, X], axis=0)
+        ys = pd.concat([ys, y], axis=0)
+
+    Xs.to_csv(input_path / f'fold_{f}_vid_{v}.csv', index_label='time')
+    ys.to_csv(output_path / f'fold_{f}_vid_{v}.csv', index_label='time')
+    
+specific_func = partial(func, 
+                        input_path=input_path,
+                        output_path=output_path,
+                        past_window_size=past_window_size,
+                        future_window_size=future_window_size)
+
+pool_obj = multiprocessing.Pool()
+pool_obj.map(specific_func, model_list)
+pool_obj.close()
