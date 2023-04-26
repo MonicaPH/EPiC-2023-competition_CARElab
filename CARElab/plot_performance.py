a4183a10fca44b7f525008e8904ebce7cd6f95de
Felix <felix.dollack@googlemail.com>
2023-04-26 21:39:07 +0200

    Add script to plot lstm test performance output

---
+++ CARElab/plot_performance.py
@@ -0,0 +1,20 @@
+import argparse
+from matplotlib import pylab as plt
+import pandas as pd
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='Performance CSV file')
+    args = parser.parse_args()
+
+    df = pd.read_csv(args.file)
+
+    sensor = args.file.split('_')[1].split('.')[0]
+
+    plt.plot(df.offset/1000, df.rmse, '-o')
+    plt.ylabel('RMSE')
+    plt.xlabel('Delay [s]')
+    plt.grid()
+    plt.title('All Signals')
+    plt.savefig(f'performance_{sensor}.png')
