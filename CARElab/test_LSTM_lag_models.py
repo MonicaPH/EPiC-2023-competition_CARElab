324c93af0dd795ce1f5fc2000bf2ec056b102d9a
Felix <felix.dollack@googlemail.com>
2023-04-27 17:00:49 +0200

    Add sensor all and plotting to test lstm script

--- CARElab/test_LSTM_lag_models.py
+++ CARElab/test_LSTM_lag_models.py
@@ -2,7 +2,12 @@ import os
 
 if __name__ == '__main__':
     # test all models
-    for sensor in ['ecg', 'bvp', 'gsr', 'rsp', 'skt', 'emg_zygo', 'emg_coru', 'emg_trap']:
+    for sensor in ['ecg', 'bvp', 'gsr', 'rsp', 'skt', 'emg_zygo', 'emg_coru', 'emg_trap', 'all']:
         cmd = f'python3 test_LSTM.py {sensor} szn3_fold1_testForLSTM.csv'
+        print(cmd)
+        os.system(cmd)
+
+        # create plot
+        cmd = f'python3 plot_performance.py performance_{sensor}.csv'
         print(cmd)
         os.system(cmd)
